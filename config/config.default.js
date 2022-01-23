e024e973502ea930ee0dc4284be2e7d4e6fff679
zhengbao.xin <zhengbao.xzb@xier-inc.com>
2022-01-23 16:27:53 +0800

    feat(local):发布上传

--- config/config.default.js
+++ config/config.default.js
@@ -57,10 +57,10 @@ module.exports = (appInfo) => {
   config.oss = {
     // 这里需要的东西去自己的服务器里看，我用的阿里云
     client: {
-      accessKeyId: "LTAI5tGAaZ49iG74WwZ6H92q",
-      accessKeySecret: "1TDeDRHMXEt5Q1ltIJcNfeu0apgUhC",
-      bucket: "ronaldoxzb",
-      endpoint: "oss-cn-beijing.aliyuncs.com",
+      accessKeyId: "*****",
+      accessKeySecret: "*****",
+      bucket: "*****",
+      endpoint: "******",
       timeout: "300s",
     },
   };
